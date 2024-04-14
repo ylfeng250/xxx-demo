8e7ca80234bbc229ed3c0f7c7e52d9386c6b3401
六奇 <1119030015@qq.com>
2024-04-14 23:49:53 +0800

    link

--- ice.config.mts
+++ ice.config.mts
@@ -9,5 +9,4 @@ export default defineConfig(() => ({
     onDemand: true,
     format: "esm",
   },
-  hash: "contenthash",
 }));
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
+import { Link } from "@ice/runtime";
+
 export default function Home() {
   return (
     <div>
       <h1>一些 Demo</h1>
       <ul>
         <li>
-          <a href="/checkUpdate" target="_blank">
+          <Link to="/checkUpdate" target="_blank">
             检查页面是否更新
-          </a>
+          </Link>
         </li>
       </ul>
     </div>
