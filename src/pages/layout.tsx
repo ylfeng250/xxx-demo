3d9afabfbdb217c8cb752bf3db4a63b7218ae99a
六奇 <1119030015@qq.com>
2024-05-03 11:23:04 +0800

    histroy

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,8 +1,16 @@
 import { Outlet } from "@ice/runtime";
 import usePageChange from "@/hook/usePageChange";
+import { useEffect } from "react";
 
 export default function Layout() {
-  usePageChange();
+  //   usePageChange();
+  useEffect(() => {
+    // @ts-expect-error
+    navigation.addEventListener("navigate", (props) => {
+      console.log("page changed");
+      console.log(props.destination.url);
+    });
+  }, []);
   return (
     <div>
       nihao
